8761780abb6c84ebcd3f22b3aa4290cd02fee79a
Mariusz Bielesz <bielesz@yahoo.com>
2022-09-08 12:55:48 +0200

    New recipe app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dee85be7-8556-4fed-a47b-045f6c832a3d';
+export const MODAL_CLOSE_SEC = 2.5;
